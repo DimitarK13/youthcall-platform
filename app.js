f30b53137abe54e4fba79e6919464e9c4531e2ce
Dimitar <dimikalapocev@gmail.com>
2023-09-26 18:05:12 +0200

    Create public folder

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 7777;
 const home = require('./routes/home');
 
+app.use(express.static('./public'));
+
 app.use('/', home);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
