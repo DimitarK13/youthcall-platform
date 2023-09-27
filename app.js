de59a85cf43643aa96e8fc3f954e699eae7e2fb3
Dimitar <dimikalapocev@gmail.com>
2023-09-27 19:37:36 +0200

    Handle get project count

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const port = 7777;
 const home = require('./routes/home');
 
 app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use('/', home);
+app.use('/api/data', home);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
