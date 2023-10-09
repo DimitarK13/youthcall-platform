3f5f2f9d8ea3c452175c9e661dd7a5cb4020d7d6
Dimitar <dimikalapocev@gmail.com>
2023-10-09 17:36:30 +0200

    Get posts from database

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,10 +1,57 @@
-const { data, allPosts } = require('../testData');
+const { allPosts } = require('../testData');
+const mongoose = require('mongoose');
 
-const getPosts = (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: data,
-  });
+const postSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  where: {
+    type: String,
+    required: true,
+  },
+  when: {
+    type: String,
+    required: true,
+  },
+  who: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  deadline: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({
+      success: true,
+      data: posts,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const newPost = (req, res) => {
@@ -36,12 +83,10 @@ const newPost = (req, res) => {
 
   console.log(newPost);
 
-  res
-    .status(200)
-    .json({
-      succes: true,
-      data: `Post (${name}, id: ${id}) successfully created`,
-    });
+  res.status(200).json({
+    succes: true,
+    data: `Post (${name}, id: ${id}) successfully created`,
+  });
 };
 
 const deletePost = (req, res) => {
@@ -52,6 +97,7 @@ const deletePost = (req, res) => {
       .status(404)
       .json({ success: false, msg: `no post with id ${req.params.id}` });
   }
+
   const newPosts = allPosts.filter((post) => post.id !== Number(req.params.id));
 
   return res.status(200).json({ success: true, data: newPosts });
