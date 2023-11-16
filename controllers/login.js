c0bd72dd306d79b9df9afcf2a0ba04482a17ab6c
Dimitar <dimikalapocev@gmail.com>
2023-11-16 19:17:30 +0100

    implement login on frontend

--- controllers/login.js
+++ controllers/login.js
@@ -6,26 +6,29 @@ const login = async (req, res) => {
     const accounts = await Account.find({});
 
     if (!username || !password)
-      return res
-        .status(406)
-        .json({ success: false, data: 'Please enter username and password' });
+      return res.status(406).json({
+        success: false,
+        data: 'Задолжително корисничко име и лозинка',
+      });
 
     const account = accounts.find((acc) => acc.username === username);
 
     if (!account)
       return res.status(401).json({
         success: false,
-        data: `Account with username: ${username} doesn't exist. Please check username`,
+        data: `Акаунт со корисничко име: ${username} не постои. Ве молиме обидете се повторно!`,
       });
 
     if (account.password !== password)
-      return res
-        .status(401)
-        .json({ success: false, data: 'The password is incorect' });
+      return res.status(401).json({ success: false, data: 'Погрешна лозинка' });
 
     res.status(200).json({
       success: true,
-      data: `${account.name} (${account.username}) Logged In`,
+      data: {
+        loggedIn: true,
+        username,
+        accountName: account.name,
+      },
     });
   } catch (err) {
     console.error(err);
