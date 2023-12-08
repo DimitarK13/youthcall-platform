f0d7455dbf75418a2a91807f47265e7bb5e6f6ef
Dimitar <dimikalapocev@gmail.com>
2023-12-08 15:08:12 +0100

    (mongoose): add required field

--- models/Account.js
+++ models/Account.js
@@ -3,15 +3,19 @@ const mongoose = require('mongoose');
 const accountSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
   },
   username: {
     type: String,
+    required: true,
   },
   password: {
     type: String,
+    required: true,
   },
   website: {
     type: String,
+    required: true,
   },
 });
 
--- models/Post.js
+++ models/Post.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
   type: {
     type: String,
+    required: true,
   },
   name: {
     type: String,
+    required: true,
   },
   image: {
     data: Buffer,
@@ -13,30 +15,39 @@ const postSchema = new mongoose.Schema({
   },
   where: {
     type: String,
+    required: true,
   },
   when: {
     type: String,
+    required: true,
   },
   who: {
     type: String,
+    required: true,
   },
   link: {
     type: String,
+    required: true,
   },
   deadline: {
     type: String,
+    required: true,
   },
   description: {
     type: String,
+    required: true,
   },
   username: {
     type: String,
+    required: true,
   },
   website: {
     type: String,
+    required: true,
   },
   accountName: {
     type: String,
+    required: true,
   },
 });
 
